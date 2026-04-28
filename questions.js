0f60c28f964ca0aced002c49b0539ca0a47ed5f3
Paul-Sayles <saylesp@laurelacademy.org.uk>
2026-04-28 10:11:49 +0100

    Add QUESTION_BANK with materials questions

---
+++ questions.js
@@ -0,0 +1,20 @@
+const QUESTION_BANK = {
+  units: [
+    {
+      title: "OCR Engineering J823 - Materials",
+      topics: [
+        {
+          title: "Material Properties",
+          questions: [
+            {
+              question: "Which property describes a material's ability to be stretched into wire?",
+              options: ["Hardness", "Ductility", "Toughness", "Elasticity"],
+              correct: 1,
+              explanation: "Ductility is the ability of a material to be drawn or stretched into wire."
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
